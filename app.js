cc6ae796ef527b41ea4cac628f03e02003efe72a
Prateek Chaurasia <prateek18801@gmail.com>
2022-07-26 12:20:06 +0530

    add home route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.status(200).sendFile(__dirname + '/index.html');
+});
+
 app.listen(process.env.PORT, process.env.HOST, () => {
     console.log(`server running on ${process.env.HOST}:${process.env.PORT}`);
 });
